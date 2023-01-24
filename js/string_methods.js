b7cf65fff9d804c0d46c1141984406448f812c1c
srikanthpragada <srikanthpragada@gmail.com>
2023-01-24 17:56:14 +0530

    24-JAN

---
+++ js/string_methods.js
@@ -0,0 +1,16 @@
+
+let st = "JavaScript"
+
+console.log(st.indexOf("Script"))
+console.log(st[0])
+console.log(st.charAt(0))
+
+console.log(st.includes("Script"))
+
+console.log("20,30,40".split(","))
+
+console.log(st.substring(4))
+console.log(st.length)
+
+st[0] = 'j'
+console.log(st)
