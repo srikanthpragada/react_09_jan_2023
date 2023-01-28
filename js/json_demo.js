f5d24b6b92356d1bcfdb6b9a7db29660e95e81c0
srikanthpragada <srikanthpragada@gmail.com>
2023-01-28 18:57:44 +0530

    28-JAN

---
+++ js/json_demo.js
@@ -0,0 +1,8 @@
+var c = {title : "React", price : 3000}
+
+var json = JSON.stringify(c)  // JS to JSON
+console.log(json)
+
+var js = JSON.parse(json)   // JSON to JS
+console.log(js)
+
---
+++ js/try_demo.js
@@ -0,0 +1,17 @@
+p = { name : 'Abc'}
+a = []
+
+console.log(p.ename)
+console.log(a[0])
+v = 10 / 0 
+console.log(v)
+
+try  {
+  fun()   // throw exception 
+  console.log("Done") 
+}
+catch(err)  {
+  console.log("Error : " + err)
+}
+
+console.log("The End")
